09fc990f5d9a6864477d303baef132eb80337138
p0isonx <46392370+p0isonxs@users.noreply.github.com>
2025-06-18 20:22:23 +0700

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import TetrisGame from "./TetrisGame";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <TetrisGame />
-  </React.StrictMode>
-);
