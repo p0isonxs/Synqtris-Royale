ea7d111382ce39055b3f771bc09f6cc11d1a3688
p0isonx <46392370+p0isonxs@users.noreply.github.com>
2025-06-19 21:14:00 +0700

    Delete pages directory

--- pages/index.js
+++
@@ -1,7 +0,0 @@
-import dynamic from "next/dynamic";
-
-const SynqtrisGame = dynamic(() => import("../components/SynqtrisGame"), { ssr: false });
-
-export default function Home() {
-  return <SynqtrisGame />;
-}
