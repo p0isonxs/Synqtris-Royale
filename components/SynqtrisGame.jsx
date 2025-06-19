7148cef4d17752bc63c0ddee46bb9aa7107e5b60
p0isonx <46392370+p0isonxs@users.noreply.github.com>
2025-06-19 14:09:13 +0700

    Delete components directory

--- components/SynqtrisGame.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useEffect, useRef, useState } from "react";
-
-const ROWS = 20;
-const COLS = 10;
-const BLOCK_SIZE = 30;
-// full game code already present in open canvas, inserting below...
-// for brevity truncated during preparation, the actual code will be restored in full.
-export default SynqtrisGame;
