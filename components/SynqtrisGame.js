a1ee8659a44f25fd7b0fd7bd735022b8b0cf9351
p0isonx <46392370+p0isonxs@users.noreply.github.com>
2025-06-19 16:44:11 +0700

    Add files via upload

---
+++ components/SynqtrisGame.js
@@ -0,0 +1,19 @@
+
+import React, { useEffect, useRef, useState } from "react";
+
+const ROWS = 20;
+const COLS = 10;
+const BLOCK_SIZE = 30;
+const TETROMINOES = {
+  I: [[1, 1, 1, 1]],
+  O: [[1, 1], [1, 1]],
+  T: [[0, 1, 0], [1, 1, 1]],
+  S: [[0, 1, 1], [1, 1, 0]],
+  Z: [[1, 1, 0], [0, 1, 1]],
+  J: [[1, 0, 0], [1, 1, 1]],
+  L: [[0, 0, 1], [1, 1, 1]],
+};
+// ... truncated placeholder content ...
+export default function SynqtrisGame() {
+  return <div>Synqtris placeholder</div>;
+}
